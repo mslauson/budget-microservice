4183416963498aed5063b730c73a45450b017397
Matthew Slauson <slauson.matthew@gmail.om>
2020-07-05 09:34:14 -0500

    add mapper method for updating the budget entity

--- src/main/java/io/blossombudgeting/microservices/budget/util/BudgetMapper.java
+++ src/main/java/io/blossombudgeting/microservices/budget/util/BudgetMapper.java
@@ -6,6 +6,7 @@
 package io.blossombudgeting.microservices.budget.util;
 
 import io.blossombudgeting.microservices.budget.domain.models.BudgetBase;
+import io.blossombudgeting.microservices.budget.domain.models.UpdateBudgetRequestModel;
 import io.blossombudgeting.util.budgetcommonutil.entity.BudgetEntity;
 import io.blossombudgeting.util.budgetcommonutil.util.DateUtils;
 import org.springframework.stereotype.Component;
@@ -44,6 +45,20 @@ public class BudgetMapper {
                 .visible(budgetEntity.isVisible())
                 .linkedTransactions(budgetEntity.getLinkedTransactions())
                 .build();
+    }
+
+    public BudgetEntity updateCurrentEntity(UpdateBudgetRequestModel request, BudgetEntity entity) {
+        entity.setDateCreated(request.getDateCreated() != null ? request.getDateCreated() : DateUtils.utcTimeStamp());
+        entity.setMonthYear(request.getMonthYear());
+        entity.setName(request.getName());
+        entity.setCategory(request.getCategory());
+        entity.setSubCategory(request.getSubCategory());
+        entity.setUsed(request.getUsed());
+        entity.setAllocation(request.getAllocation());
+        entity.setVisible(request.isVisible());
+        entity.setLinkedTransactions(request.getLinkedTransactions());
+
+        return entity;
     }
 
 }
