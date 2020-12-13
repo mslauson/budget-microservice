5eabb540ff491bd21d089874b633723b23a46d9e
matthewslauson <slauson.matthew@gmail.com>
2020-12-13 11:35:39 -0600

    add tests

--- src/main/java/io/blossombudgeting/microservices/budget/domain/models/RemoveTransactionsRequestModel.java
+++ src/main/java/io/blossombudgeting/microservices/budget/domain/models/RemoveTransactionsRequestModel.java
@@ -2,18 +2,22 @@ package io.blossombudgeting.microservices.budget.domain.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RemoveTransactionsRequestModel {
 
     @NotBlank
-    private final String phone;
+    private String phone;
 
     @NotNull
-    private final List<String> transactionIds;
+    private List<String> transactionIds;
 }
