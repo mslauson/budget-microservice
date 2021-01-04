8e8a24e01b1b8847e8dea6edd8b801f6892a4c9e
matthewslauson <slauson.matthew@gmail.com>
2021-01-04 09:28:02 -0600

    create private method for creating the new budget entities

--- src/main/java/io/blossombudgeting/microservices/budget/util/BudgetScheduledUtility.java
+++ src/main/java/io/blossombudgeting/microservices/budget/util/BudgetScheduledUtility.java
@@ -4,9 +4,11 @@ package io.blossombudgeting.microservices.budget.util;
 import io.blossombudgeting.microservices.budget.repository.BudgetRepository;
 import io.blossombudgeting.util.budgetcommonutil.entity.BudgetEntity;
 import io.blossombudgeting.util.budgetcommonutil.util.DateUtils;
+import io.blossombudgeting.util.budgetcommonutil.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Slf4j
@@ -18,5 +20,31 @@ public class BudgetScheduledUtility {
     public void reCreateBudgets(){
         Set<BudgetEntity> budgetEntities = budgetRepository.findAllByMonthYear(String.valueOf(DateUtils.getFirstOfMonth()));
 
+    }
+
+    /**
+     * Creates new budget entities that have the same properties as last months
+     *
+     * @param budgetEntities to copy
+     * @return new months budgets
+     */
+    private Set<BudgetEntity> updateEntities(Set<BudgetEntity> budgetEntities){
+        Set<BudgetEntity> nextMonthEntities = new HashSet<>();
+        String newMonthYear = String.valueOf(DateUtils.getFirstOfMonth().plusMonths(1));
+        budgetEntities.forEach(budgetEntity -> {
+            String category = budgetEntity.getCategory();
+            nextMonthEntities.add(
+            BudgetEntity
+                    .builder()
+                    .id(StringUtils.buildStringBuffer(category, newMonthYear, budgetEntity.getPhone()))
+                    .name(category)
+                    .category(category)
+                    .allocation(budgetEntity.getAllocation())
+                    .monthYear(newMonthYear)
+                    .build()
+            );
+        });
+
+        return nextMonthEntities;
     }
 }
