ea72b960c17ed07781c1971084c6a8792c3ab0a2
matthewslauson <slauson.matthew@gmail.com>
2021-01-09 10:02:34 -0600

    add decryption

--- src/main/java/io/blossombudgeting/microservices/budget/util/BudgetMapper.java
+++ src/main/java/io/blossombudgeting/microservices/budget/util/BudgetMapper.java
@@ -28,16 +28,16 @@ public class BudgetMapper {
 
     public BudgetEntity covertToEntity(BudgetBase request) {
         return BudgetEntity.builder()
-                .phone(request.getPhone().toLowerCase())
+                .phone(encryptionUtility.decryptData(request.getPhone().toLowerCase().getBytes(StandardCharsets.UTF_8)).toString())
                 .dateCreated(request.getDateCreated() != null ? request.getDateCreated() : DateUtils.utcTimeStamp())
-                .monthYear(request.getMonthYear())
-                .name(request.getName())
-                .category(request.getCategory())
-                .subCategory(request.getSubCategory())
+                .monthYear(encryptionUtility.decryptData(request.getMonthYear().getBytes(StandardCharsets.UTF_8)).toString())
+                .name(encryptionUtility.decryptData(request.getName().getBytes(StandardCharsets.UTF_8)).toString())
+                .category(encryptionUtility.decryptData(request.getCategory().getBytes(StandardCharsets.UTF_8)).toString())
+                .subCategory(decryptSubCategory(request.getSubCategory()))
                 .used(request.getUsed())
                 .allocation(request.getAllocation())
                 .visible(request.isVisible())
-                .linkedTransactions(request.getLinkedTransactions())
+                .linkedTransactions(decryptLinkedTransactions(request.getLinkedTransactions()))
                 .build();
     }
 
@@ -59,14 +59,14 @@ public class BudgetMapper {
 
     public BudgetEntity updateCurrentEntity(UpdateBudgetRequestModel request, BudgetEntity entity) {
         entity.setDateCreated(request.getDateCreated() != null ? request.getDateCreated() : DateUtils.utcTimeStamp());
-        entity.setMonthYear(request.getMonthYear());
-        entity.setName(request.getName());
-        entity.setCategory(request.getCategory());
-        entity.setSubCategory(request.getSubCategory());
+        entity.setMonthYear(encryptionUtility.decryptData(request.getMonthYear().getBytes(StandardCharsets.UTF_8)).toString());
+        entity.setName(encryptionUtility.decryptData(request.getName().getBytes(StandardCharsets.UTF_8)).toString());
+        entity.setCategory(encryptionUtility.decryptData(request.getCategory().getBytes(StandardCharsets.UTF_8)).toString());
+        entity.setSubCategory(decryptSubCategory(request.getSubCategory()));
         entity.setUsed(request.getUsed());
         entity.setAllocation(request.getAllocation());
         entity.setVisible(request.isVisible());
-        entity.setLinkedTransactions(request.getLinkedTransactions());
+        entity.setLinkedTransactions(decryptLinkedTransactions(request.getLinkedTransactions()));
 
         return entity;
     }
@@ -91,6 +91,32 @@ public class BudgetMapper {
         linkedTransactions.forEach(linkedTransaction -> {
             linkedTransaction.setTransactionId(
                     encryptionUtility.encryptData(linkedTransaction.getTransactionId().getBytes(StandardCharsets.UTF_8)).toString()
+            );
+        });
+        return linkedTransactions;
+    }
+
+
+    private List<SubCategoryDocument> decryptSubCategory(List<SubCategoryDocument> subCategoryDocuments){
+        subCategoryDocuments.forEach(subCategoryDocument -> {
+        encryptLinkedTransactions(subCategoryDocument.getLinkedTransactions());
+        subCategoryDocument.setCategory(
+                encryptionUtility.decryptData(subCategoryDocument.getCategory().getBytes(StandardCharsets.UTF_8)).toString()
+        );
+        subCategoryDocument.setName(
+                encryptionUtility.decryptData(subCategoryDocument.getName().getBytes(StandardCharsets.UTF_8)).toString()
+        );
+        subCategoryDocument.setId(
+                encryptionUtility.decryptData(subCategoryDocument.getId().getBytes(StandardCharsets.UTF_8)).toString()
+        );
+        });
+        return subCategoryDocuments;
+    }
+
+    private List<LinkedTransactions> decryptLinkedTransactions(List<LinkedTransactions> linkedTransactions){
+        linkedTransactions.forEach(linkedTransaction -> {
+            linkedTransaction.setTransactionId(
+                    encryptionUtility.decryptData(linkedTransaction.getTransactionId().getBytes(StandardCharsets.UTF_8)).toString()
             );
         });
         return linkedTransactions;
