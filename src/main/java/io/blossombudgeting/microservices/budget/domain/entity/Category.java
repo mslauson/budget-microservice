71471ecbbb7a1b8fcec19b640688186682d02b5f
matthewslauson <slauson.matthew@gmail.com>
2021-02-08 18:32:40 -0600

    create new entities

---
+++ src/main/java/io/blossombudgeting/microservices/budget/domain/entity/Category.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021. Blossom Budgeting LLC
+ * All Rights Reserved
+ */
+
+package io.blossombudgeting.microservices.budget.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    private String id;
+    private String category;
+}
---
+++ src/main/java/io/blossombudgeting/microservices/budget/domain/entity/DefaultCategoriesEntity.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021. Blossom Budgeting LLC
+ * All Rights Reserved
+ */
+
+package io.blossombudgeting.microservices.budget.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@Document("default-categories")
+public class DefaultCategoriesEntity {
+
+    @Id
+    private String id;
+    private List<Category> categories;
+}
