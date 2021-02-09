331a48cadb6dd4cd4369dec189495925a95e0b6a
matthewslauson <slauson.matthew@gmail.com>
2021-02-09 17:05:28 -0600

    create repo

--- src/main/java/io/blossombudgeting/microservices/budget/domain/models/CategoriesModel.java
+++ src/main/java/io/blossombudgeting/microservices/budget/domain/models/CategoriesModel.java
@@ -5,6 +5,7 @@
 
 package io.blossombudgeting.microservices.budget.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,9 +19,11 @@ import java.util.List;
 @Setter
 @Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CategoriesModel {
 
     private String id;
+    private String phone;
     @Valid
     @NotNull
     private List<Category> categories;
---
+++ src/main/java/io/blossombudgeting/microservices/budget/repository/CustomerCategoriesRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. Blossom Budgeting LLC
+ * All Rights Reserved
+ */
+
+package io.blossombudgeting.microservices.budget.repository;
+
+import io.blossombudgeting.microservices.budget.domain.entity.CustomerCategoriesEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerCategoriesRepository extends MongoRepository<CustomerCategoriesEntity, String> {
+}
